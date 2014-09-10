fdd2d0a3cd2fcafb57eb3f6c4f64b10c1d20c6bf
Borislav Angelov <borislav.angelov@gmail.com>
2014-09-10 03:17:24 +0300

    Add upload multipart

--- examples/UploadFile.php
+++ examples/UploadFile.php
@@ -2,12 +2,12 @@
 
 include_once '../lib/GdriveClient.php';
 
-$accessToken = '';
+$accessToken = 'ya29.ewCtW6c8loihzkUSwTn3W2SPzR68zbTx1LVVRKFVek7aOzM6vUIrkGMa';
 
 $client = new GdriveClient($accessToken);
 
 $file = fopen('./bmw.jpg', 'rb');
-$response = $client->uploadFile('/Photos/Car.jpg', $file );
+$response = $client->uploadFile('Car.jpg', $file, filesize('./bmw.jpg'));
 fclose($file);
 
 print_r($response);
